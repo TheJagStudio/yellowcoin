73737513e07de17add42cde9d387d350b170cbd9
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2022-06-04 10:11:07 +0530

    Real Api

--- yellowcoin/asgi.py
+++ yellowcoin/asgi.py
@@ -7,16 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.urls import path
+from trading.routing import ws_urlpatterns
 from django.core.asgi import get_asgi_application
 from . import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
 })
