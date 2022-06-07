ced3f8c1520c57a2e55d592e8549da138789bea5
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2022-06-07 21:11:58 +0530

    chrome update

--- trading/views.py
+++ trading/views.py
@@ -68,8 +68,8 @@ def access_gen(request):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(
-        options=chrome_options, executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+    driver = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(kite.login_url())
     sleep(2)
     password = driver.find_element(By.ID, "userid")
