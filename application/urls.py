d633cda67c201df89173826826a0bfdd64cf91bd
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2022-06-12 19:06:39 +0530

    application

--- application/urls.py
+++ application/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'application'
 urlpatterns = [
     path('login/', views.login_user, name='login_user'),
-    path('get_user/', views.get_user, name='get_user'),
+    path('segmentData/', views.segmentData, name='segmentData'),
 ]
--- application/views.py
+++ application/views.py
@@ -5,7 +5,30 @@ from django.contrib.auth.models import User
 import json
 # import csrf
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from user.models import UserAccount
+import csv
+from accounts.models import stack
+from trading.views import ApiF
+
+MCX = []
+NSE = []
+NFO = []
+MCXS = []
+NSES = []
+NFOS = []
+with open("static/all_Symbols.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        if row[11] == "MCX":
+            MCX.append(row)
+            MCXS.append(row[2])
+        elif row[11] == "NSE" and row[3] != "":
+            NSE.append(row)
+            NSES.append(row[3])
+        elif row[11] == "NFO" and row[3] != "":
+            NFO.append(row)
+            NFOS.append(row[3])
 
 
 @csrf_exempt
@@ -27,12 +50,6 @@ def login_user(request):
 
 
 @csrf_exempt
-def get_user(request):
-    if request.user.is_authenticated:
-        userType = UserAccount.objects.filter(
-            user=request.user).first().Account_Type
-        data = {
-            "name": request.user.username,
-            "type": userType
-        }
-        return HttpResponse(json.dumps(data, indent=4), content_type="application/json")
+def segmentData(request):
+    data = {"NSE": NSES, "NFO": NFOS, "MCX": MCXS}
+    return HttpResponse(json.dumps(data, indent=4), content_type="application/json")
